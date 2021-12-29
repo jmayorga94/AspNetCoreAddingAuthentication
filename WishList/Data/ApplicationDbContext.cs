44f0a98f413ccb5adb83967800c6180883316e2a
jmayorga94 <javi.mayorga1994@outlook.com>
2021-12-29 11:55:42 -0600

    Completed module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
